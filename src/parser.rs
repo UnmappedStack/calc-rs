4f9df123d3ba97eec9ee25cd1769d3daaa3e5567
UnmappedStack <jake.steinburger0@gmail.com>
2024-11-02 18:39:39 +1100

    Use token slices instead of vector

--- src/parser.rs
+++ src/parser.rs
@@ -32,7 +32,7 @@ pub fn print_ast(root: &Branch) {
 }
 
 /* parses a single branch recursively */
-fn parse_branch(tokens: &Vec<tokeniser::Token>, priorities: &mut HashMap<tokeniser::TokenType, u8>) -> Box<Branch> {
+fn parse_branch(tokens: &[tokeniser::Token], priorities: &mut HashMap<tokeniser::TokenType, u8>) -> Box<Branch> {
     // if the whole node is one single token (must be of type NUM), then return a num node
     if tokens.len() == 1 {
         if tokens[0].ttype == tokeniser::TokenType::NUM {
@@ -55,26 +55,10 @@ fn parse_branch(tokens: &Vec<tokeniser::Token>, priorities: &mut HashMap<tokenis
             }
         }
     }
-    /* create a vector of the nodes on either side of this max priority token, and pass them recursively into parse_branch. 
-     * take the output branch and form this branch with either side.
-     * You may wonder why I don't just slice the vector. It's because a slice is read-only, and I need this
-     * to be mutable.
-     */
-    // First on the left side
-    let mut left_tokens: Vec<tokeniser::Token> = Vec::new();
-    for idx in 0..max_priority_token_idx {
-        left_tokens.push(tokens[idx]);
-    }
-    let left_branch = parse_branch(&left_tokens, priorities);
-    // now the right side
-    let mut right_tokens: Vec<tokeniser::Token> = Vec::new();
-    max_priority_token_idx += 1;
-    for idx in max_priority_token_idx..tokens_len {
-        right_tokens.push(tokens[idx]);
-    }
-    let right_branch = parse_branch(&right_tokens, priorities);
+    let left_branch = parse_branch(&tokens[..max_priority_token_idx], priorities);
+    let right_branch = parse_branch(&tokens[(max_priority_token_idx + 1)..], priorities);
     // Now it can finally create this branch, and return it in a box
-    Box::new(Branch {is_num_node: false, val: 0.0, operation: tokens[max_priority_token_idx - 1].ttype, left: Some(left_branch), right: Some(right_branch)})
+    Box::new(Branch {is_num_node: false, val: 0.0, operation: tokens[max_priority_token_idx].ttype, left: Some(left_branch), right: Some(right_branch)})
 }
 
 /* creates the whole AST - sets up the priority hashmap then starts the recursive parsing of
@@ -88,5 +72,5 @@ pub fn parse(tokens: &mut Vec<tokeniser::Token>) -> Branch {
         (tokeniser::TokenType::SUB, 3),
         (tokeniser::TokenType::ADD, 3),
     ]);
-    *parse_branch(&tokens, &mut priorities_map)
+    *parse_branch(&tokens[..], &mut priorities_map)
 }
